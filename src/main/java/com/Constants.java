f7409be0306c9a07deab83fdd2ab577f84d658b9
hekf2021 <hekf2021@126.com>
2018-01-10 17:06:36 +0800

    修改

--- src/main/java/com/Constants.java
+++ src/main/java/com/Constants.java
@@ -4,6 +4,6 @@ package com;
  * Created by Administrator on 2018/1/10.
  */
 public class Constants {
-    public static final String zookeeper="172.16.50.21:2181";
-    public static final String kafka="172.16.50.21:9092";
+    public static final String zookeeper="ip:2181";
+    public static final String kafka="ip:9092";
 }
